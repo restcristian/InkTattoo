7e38a12f9c81051aa62881b4b5b195da510d7a53
SDQ\cristian.restituyo <crestituyo@theoreminc.net>
2018-02-08 10:54:30 -0400

    Added mobile functionality for Navigation Menu <NavItems />

--- src/components/NavItems/NavItems.js
+++ src/components/NavItems/NavItems.js
@@ -4,16 +4,21 @@ import Logo from '../../assets/imgs/logo.png';
 
 class NavItems extends Component {
     state = {
-        pastScrolled: false
+        pastScrolled: false,
+        isMobile: false,
+        isMobileOpen: false,
     }
     componentDidMount() {
+        this.resizeHandler();
         window.addEventListener('scroll', this.scrollHandler);
+        window.addEventListener('resize', this.resizeHandler);
     }
     shouldComponentUpdate(nextProps, nextState) {
-        return nextState.pastScrolled !== this.state.pastScrolled;
+        return nextState.pastScrolled !== this.state.pastScrolled ||
+            nextState.isMobile !== this.state.isMobile ||
+            nextState.isMobileOpen !== this.state.isMobileOpen;
     }
     scrollHandler = (e) => {
-       
         let clientHeight = this.navBar.clientHeight;
         let offset = 80;
         if (window.pageYOffset > (clientHeight - offset)) {
@@ -21,7 +26,17 @@ class NavItems extends Component {
         } else {
             this.setState({ pastScrolled: false });
         }
-        
+    }
+    resizeHandler = (e) => {
+        if (window.innerWidth <= 768) {
+            this.setState({ isMobile: true });
+        } else {
+            this.setState({ isMobile: false });
+        }
+        console.log('resizing');
+    }
+    closeMobileHandler = (e) =>{
+        this.setState({isMobileOpen:false});
     }
     render() {
         const ListItems = [
@@ -51,17 +66,36 @@ class NavItems extends Component {
             },
         ];
         let classes = ['NavItems'];
+        let navList = (
+            <ul className="NavItems--list reset-list">
+                {ListItems.map((item, idx) =>
+                    <li key={item.caption}><a href={item.url}>{item.caption}</a></li>
+                )}
+            </ul>
+        );
         if (this.state.pastScrolled) {
             classes.push('scrolled');
+        }
+        if (this.state.isMobile) {
+            classes.push('NavItems--isMobile');
+            navList = (
+                <div className = "NavItems--isMobile__wrapper">
+                    <ul className="NavItems--list--mobile reset-list">
+                        {ListItems.map((item, idx) =>
+                            <li key={item.caption}><a href={item.url}><span>{item.caption}</span></a></li>
+                        )}
+                    </ul>
+                    <button onClick = {this.closeMobileHandler} className="NavItems--isMobile__closebtn appereance"><span className = "accessible-hide">Close Menu</span><span>X</span></button>
+                </div>
+            );
+        }
+        if (this.state.isMobile && this.state.isMobileOpen) {
+            classes.push('NavItems--isMobile--open');
         }
         return (
             <nav className={classes.join(' ')} ref={(ref) => this.navBar = ref}>
                 <div className="NavItems__wrapper">
-                    <ul className="NavItems--list reset-list">
-                        {ListItems.map((item, idx) =>
-                            <li key={item.caption}><a href={item.url}>{item.caption}</a></li>
-                        )}
-                    </ul>
+                    {navList}
                     <a href="/index.html" className="main-logo">
                         <img src={Logo} alt="Ink Tatto Logo" />
                     </a>
